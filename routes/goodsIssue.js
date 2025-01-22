2ba8a886539f50c20afae688db3fd3a206fc4b63
RaditAtalla <radityaderachmadie@gmail.com>
2025-01-22 20:24:41 +0700

    FEAT: download goods issue

--- routes/goodsIssue.js
+++ routes/goodsIssue.js
@@ -1,87 +1,99 @@
 const express = require("express");
 const { PrismaClient } = require("@prisma/client");
-const { body } = require("express-validator")
+const { body } = require("express-validator");
 
 const router = express.Router();
 const prisma = new PrismaClient();
 
 router.get("/", async (req, res) => {
-  const goodsIssue = await prisma.bppb.findMany()
-  res.send(goodsIssue)
+  const bppb = await prisma.bppb.findMany({
+    include: {
+      detailBppb: {},
+    },
+  });
+  res.send(bppb);
 });
 
 router.get("/bppb/latest", async (req, res) => {
   const goodsIssue = await prisma.bppb.findFirst({
     orderBy: {
-      id: "desc"
+      id: "desc",
     },
-    take: 1
-  })
+    take: 1,
+  });
 
-  res.send(goodsIssue)
-})
+  res.send(goodsIssue);
+});
 
 router.get("/:bppbId", async (req, res) => {
   const goodsIssue = await prisma.bppb.findUnique({
     where: {
-      id: parseInt(req.params.bppbId)
+      id: parseInt(req.params.bppbId),
     },
     include: {
-      detailBppb: {}
-    }
-  })
-
-  res.send(goodsIssue)
-})
-
-router.post("/bppb", body(["namaPekerja", "lokasi"]).escape(), async (req, res) => {
-  const { jabatan, id, projectId } = req.userData
-  const { kode, materialsData, namaPekerja } = req.body
-  if (jabatan == "PM" || jabatan == "SEM") {
-    return res.send("PM dan SEM tidak bisa membuat BPPB")
-  }
-
-  const dataBppb = await prisma.bppb.create({
-    data: {
-      projectId,
-      kode,
-      namaPekerja,
-      createdByUserId: id,
-    }
-  })
-
-  materialsData.forEach(async data => {
-    await prisma.detailBppb.create({
-      data: {
-        bppbId: dataBppb.id,
-        material: data["material"],
-        spesifikasi: data["spesifikasi"],
-        volume: parseInt(data["volume"]),
-        satuan: data["satuan"],
-        lokasi: data["lokasi"],
-      }
-    })
+      detailBppb: {},
+    },
   });
 
-  res.send(dataBppb)
-})
+  res.send(goodsIssue);
+});
 
-router.post("/bppb/acc", body(["approvalStatus"]).escape(), async (req, res) => {
-  const { jabatan } = req.userData
-  const { approvalStatus, dataBppbId } = req.body
+router.post(
+  "/bppb",
+  body(["namaPekerja", "lokasi"]).escape(),
+  async (req, res) => {
+    const { jabatan, id, projectId } = req.userData;
+    const { kode, materialsData, namaPekerja } = req.body;
+    if (jabatan == "PM" || jabatan == "SEM") {
+      return res.send("PM dan SEM tidak bisa membuat BPPB");
+    }
 
-  if (jabatan == "LOGISTIK" || jabatan == "PENBAR") {
-    const acc = await prisma.bppb.update({
-      where: {
-        id: dataBppbId
-      },
+    const dataBppb = await prisma.bppb.create({
       data: {
-        accStatus: approvalStatus
-      }
-    })
+        projectId,
+        kode,
+        namaPekerja,
+        createdByUserId: id,
+      },
+    });
 
-    res.send(acc)
+    materialsData.forEach(async (data) => {
+      await prisma.detailBppb.create({
+        data: {
+          bppbId: dataBppb.id,
+          material: data["material"],
+          spesifikasi: data["spesifikasi"],
+          volume: parseInt(data["volume"]),
+          satuan: data["satuan"],
+          lokasi: data["lokasi"],
+        },
+      });
+    });
+
+    res.send(dataBppb);
   }
-})
+);
+
+router.post(
+  "/bppb/acc",
+  body(["approvalStatus"]).escape(),
+  async (req, res) => {
+    const { jabatan } = req.userData;
+    const { approvalStatus, dataBppbId } = req.body;
+
+    if (jabatan == "LOGISTIK" || jabatan == "PENBAR") {
+      const acc = await prisma.bppb.update({
+        where: {
+          id: dataBppbId,
+        },
+        data: {
+          accStatus: approvalStatus,
+        },
+      });
+
+      res.send(acc);
+    }
+  }
+);
 
 module.exports = router;
