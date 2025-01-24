45a06ede4c7756f109f4ee747edfe9dc699b59d3
RaditAtalla <radityaderachmadie@gmail.com>
2025-01-24 20:27:08 +0700

    FEAT: handle single bppb download

--- routes/goodsIssue.js
+++ routes/goodsIssue.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const { PrismaClient } = require("@prisma/client");
 const { body } = require("express-validator");
+const { PDFDocument, rgb } = require("pdf-lib");
+const fs = require("fs");
+const path = require("path");
 
 const router = express.Router();
 const prisma = new PrismaClient();
@@ -40,6 +43,131 @@ router.get("/bppb/latest", async (req, res) => {
   });
 
   res.send(goodsIssue);
+});
+
+router.get("/bppb/download/:bppbId", async (req, res) => {
+  try {
+    const bppb = await prisma.bppb.findUnique({
+      where: {
+        id: parseInt(req.params.bppbId),
+      },
+      include: {
+        detailBppb: {},
+      },
+    });
+
+    const requester = await prisma.user.findUnique({
+      where: {
+        id: bppb.createdByUserId,
+      },
+    });
+
+    const pdfPath = path.resolve(process.cwd(), "public/bppb.pdf");
+    const pdfBytes = fs.readFileSync(pdfPath);
+
+    const pdfDoc = await PDFDocument.load(pdfBytes);
+    const page = pdfDoc.getPages()[0];
+
+    const months = [
+      "Januari",
+      "Februari",
+      "Maret",
+      "April",
+      "Mei",
+      "Juni",
+      "Juli",
+      "Agustus",
+      "September",
+      "Oktober",
+      "November",
+      "Desember",
+    ];
+
+    function getMonthName(index) {
+      return months[index];
+    }
+
+    page.drawText(`${bppb.kode}`, {
+      x: 65,
+      y: 468,
+      size: 10,
+      color: rgb(0, 0, 0),
+    });
+    page.drawText("1501", {
+      x: 90,
+      y: 447,
+      size: 10,
+      color: rgb(0, 0, 0),
+    });
+    page.drawText(
+      `${bppb.createdAt.getDate()} ${getMonthName(
+        bppb.createdAt.getMonth()
+      )} ${bppb.createdAt.getFullYear()}`,
+      {
+        x: 290,
+        y: 469,
+        size: 10,
+        color: rgb(0, 0, 0),
+      }
+    );
+    page.drawText("TULT", {
+      x: 290,
+      y: 447,
+      size: 10,
+      color: rgb(0, 0, 0),
+    });
+
+    bppb.detailBppb.map((d, i) => {
+      page.drawText(`${d.lokasi}`, {
+        x: 72,
+        y: 385 - i * 16,
+        size: 10,
+        color: rgb(0, 0, 0),
+      });
+      page.drawText(`${d.material}`, {
+        x: 185,
+        y: 385 - i * 16,
+        size: 10,
+        color: rgb(0, 0, 0),
+      });
+      page.drawText(`${d.volume}`, {
+        x: 285,
+        y: 385 - i * 16,
+        size: 10,
+        color: rgb(0, 0, 0),
+      });
+      page.drawText(`${d.satuan}`, {
+        x: 350,
+        y: 385 - i * 16,
+        size: 10,
+        color: rgb(0, 0, 0),
+      });
+    });
+
+    page.drawText(`${requester.jabatan}`, {
+      x: 105,
+      y: 195,
+      size: 10,
+      color: rgb(0, 0, 0),
+    });
+    page.drawText(`${requester.nama}`, {
+      x: 70,
+      y: 140,
+      size: 10,
+      color: rgb(0, 0, 0),
+    });
+
+    const pdfData = await pdfDoc.save();
+
+    res.setHeader("Content-Type", "application/pdf");
+    res.setHeader("Content-Disposition", `attachment; filename="BPPB.pdf"`);
+    res.setHeader("Content-Length", pdfData.length);
+
+    res.end(pdfData);
+  } catch (error) {
+    console.error("Error populating PDF:", error);
+    res.status(500).send("Error populating PDF");
+  }
 });
 
 router.get("/bppb/:bppbId", async (req, res) => {
