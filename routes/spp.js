1b5c7de8dad1f50313859e1a2094a24fe788e1a7
RaditAtalla <radityaderachmadie@gmail.com>
2025-01-24 17:15:28 +0700

    FEAT: handle download single spp

--- routes/spp.js
+++ routes/spp.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const { PrismaClient } = require("@prisma/client");
 const { body } = require("express-validator");
+const { PDFDocument, rgb } = require("pdf-lib");
+const fs = require("fs");
+const path = require("path");
+const process = require("process");
 
 const router = express.Router();
 const prisma = new PrismaClient();
@@ -52,6 +56,157 @@ router.get("/latest", async (req, res) => {
   });
 
   res.send(latestSpp);
+});
+
+router.get("/download/:sppId", async (req, res) => {
+  const { projectId } = req.userData;
+  const { sppId } = req.params;
+  try {
+    const spp = await prisma.dataSpp.findUnique({
+      where: {
+        id: parseInt(sppId),
+      },
+      include: {
+        detailSpp: {},
+      },
+    });
+
+    const data = spp.detailSpp;
+
+    const project = await prisma.project.findUnique({
+      where: {
+        id: projectId,
+      },
+    });
+
+    const acc1 = await prisma.user.findUnique({
+      where: {
+        id: project.sppAcc1Id,
+      },
+    });
+
+    const acc2 = await prisma.user.findUnique({
+      where: {
+        id: project.sppAcc2Id,
+      },
+    });
+
+    const createdBy = await prisma.user.findUnique({
+      where: {
+        id: spp.createdByUserId,
+      },
+    });
+
+    function changeSlashToDash(filename) {
+      return filename.replace(/[\/\\]/g, "_");
+    }
+
+    const pdfPath = path.resolve(process.cwd(), "public/formSPP.pdf");
+    const pdfBytes = fs.readFileSync(pdfPath);
+
+    const pdfDoc = await PDFDocument.load(pdfBytes);
+    const page = pdfDoc.getPages()[0];
+
+    page.drawText(`${project.nama}`, {
+      x: 80,
+      y: 699,
+      size: 8,
+      color: rgb(0, 0, 0),
+    });
+    page.drawText(`${project.lokasi}`, {
+      x: 413,
+      y: 699,
+      size: 8,
+      color: rgb(0, 0, 0),
+    });
+    page.drawText(`${spp.kode}`, {
+      x: 413,
+      y: 688,
+      size: 8,
+      color: rgb(0, 0, 0),
+    });
+
+    data.map((d, i) => {
+      page.drawText(`${i + 1}`, {
+        x: 45,
+        y: 641 - i * 9,
+        size: 6,
+        color: rgb(0, 0, 0),
+      });
+      page.drawText(`${d.material}`, {
+        x: 75,
+        y: 641 - i * 9,
+        size: 6,
+        color: rgb(0, 0, 0),
+      });
+      page.drawText(`${d.volume}`, {
+        x: 225,
+        y: 641 - i * 9,
+        size: 6,
+        color: rgb(0, 0, 0),
+      });
+      page.drawText(`${d.satuan}`, {
+        x: 255,
+        y: 641 - i * 9,
+        size: 6,
+        color: rgb(0, 0, 0),
+      });
+      page.drawText(`${d.spesifikasi}`, {
+        x: 285,
+        y: 641 - i * 9,
+        size: 6,
+        color: rgb(0, 0, 0),
+      });
+      page.drawText(`${d.lokasi}`, {
+        x: 410,
+        y: 641 - i * 9,
+        size: 6,
+        color: rgb(0, 0, 0),
+      });
+    });
+
+    page.drawText(`${acc1.nama}`, {
+      x: 70,
+      y: 380,
+      size: 8,
+      color: rgb(0, 0, 0),
+    });
+    page.drawText(`${acc2.nama}`, {
+      x: 220,
+      y: 380,
+      size: 8,
+      color: rgb(0, 0, 0),
+    });
+    page.drawText(`${createdBy.nama}`, {
+      x: 445,
+      y: 380,
+      size: 8,
+      color: rgb(0, 0, 0),
+    });
+    page.drawText(`${createdBy.jabatan}`, {
+      x: 455,
+      y: 373,
+      size: 8,
+      color: rgb(0, 0, 0),
+    });
+
+    // Serialize the updated PDF
+    const pdfData = await pdfDoc.save();
+
+    // Set response headers
+    res.setHeader("Content-Type", "application/pdf");
+    res.setHeader(
+      "Content-Disposition",
+      `attachment; filename="${changeSlashToDash(spp.kode)}"`
+    );
+    res.setHeader("Content-Length", pdfData.length);
+
+    // Send the populated PDF
+    res.end(pdfData);
+  } catch (error) {
+    console.error("Error populating PDF:", error);
+    res.status(500).send("Error populating PDF");
+  }
 });
 
 router.get("/:sppId", async (req, res) => {
